3da299d980e1df1bd9ea20d7c1d73ecef17a0b8b
Anton Kantyshev <kaan@live.ru>
2022-04-28 16:44:43 +0300

    Add some styles. Add data to table

--- BlazorBeacon/Server/Controllers/ClassroomsController.cs
+++ BlazorBeacon/Server/Controllers/ClassroomsController.cs
@@ -55,10 +55,17 @@ namespace BlazorBeacon.Server.Controllers
                         new Lesson { Id = 1, Title = "Первый урок", Topic = "Математика", TeacherName = "Сидоров С.С.", Time = date.AddHours(-3) },
                         new Lesson { Id = 2, Title = "Второй урок", Topic = "Физика", TeacherName = "Сидоров С.С.", Time = date.AddHours(-2) },
                         new Lesson { Id = 3, Title = "Третий урок", Topic = "Математика", TeacherName = "Сидоров С.С.", Time = date.AddHours(-1) },
-                        new Lesson { Id = 4, Title = "Четвертый урок", Topic = "Математика", TeacherName = "Сидоров С.С.", Time = date, Students = new List<Student>()
+                        new Lesson { Id = 4, Title = "Четвертый урок", Topic = "Математика", TeacherName = "Андреева Т.В.", Time = date, Students = new List<Student>()
                             {
-                                new Student { Id = 1, BeaconMac = "E9-E1-79-33-78-8F", Name = "Иванов И.И.", Class = "5А" },
-                                new Student { Id = 2, BeaconMac = "FE-BB-DE-77-08-D5", Name = "Петров П.П.", Class = "6Б" },
+                                new Student { Id = 1, BeaconMac = "11-E1-79-33-78-8F", Name = "Никитин М.М.", Class = "6Б" },
+                                new Student { Id = 2, BeaconMac = "22-BB-DE-77-08-D5", Name = "Смирнов М.П.", Class = "6Б" },
+                                new Student { Id = 3, BeaconMac = "33-BB-DE-77-08-D5", Name = "Захаров С.Д.", Class = "6Б" },
+                                new Student { Id = 4, BeaconMac = "44-BB-DE-77-08-D5", Name = "Малкин А.Е.", Class = "6Б" },
+                                new Student { Id = 5, BeaconMac = "55-BB-DE-77-08-D5", Name = "Матвеев В.Т.", Class = "6Б" },
+                                new Student { Id = 6, BeaconMac = "66-BB-DE-77-08-D5", Name = "Быков Д.В.", Class = "6Б" },
+                                new Student { Id = 7, BeaconMac = "77-BB-DE-77-08-D5", Name = "Платонов О.Н.", Class = "6Б" },
+                                new Student { Id = 8, BeaconMac = "88-BB-DE-77-08-D5", Name = "Мингазов А.В.", Class = "6Б" },
+                                new Student { Id = 9, BeaconMac = "99-BB-DE-77-08-D5", Name = "Меркулов С.А.", Class = "6Б" },
                             }
                         }
                     }
